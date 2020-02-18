983bd75908f88b409b0a4465ea29f91911e6a2e4
Advincula <madvincula@dxc.com>
2020-02-18 17:09:45 +0800

    initial commit

---
+++ src/main/java/com/Splendeed/Splendeed/SplendeedApplication.java
@@ -0,0 +1,13 @@
+package com.Splendeed.Splendeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplendeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplendeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Splendeed/Splendeed/SplendeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Splendeed.Splendeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplendeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
